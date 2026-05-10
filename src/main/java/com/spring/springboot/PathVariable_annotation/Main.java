fe3b2d4ed22f92df611ad0add5b84c7b19bc3cfb
Sachin Sharma <codwork.tdotslash@gmail.com>
2026-05-10 14:31:05 +0530

    add: learn to use @PathVariable annotation. See how do we provide the PathVariable in url and how in the backend the path is exposed to the code.

--- src/main/java/com/spring/springboot/PathVariable_annotation/Main.java
+++ src/main/java/com/spring/springboot/PathVariable_annotation/Main.java
@@ -1,4 +1,12 @@
 package com.spring.springboot.PathVariable_annotation;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+    static void main() {
+        SpringApplication.run(Main.class);
+        
+    }
 }
--- src/main/java/com/spring/springboot/PathVariable_annotation/UserController.java
+++ src/main/java/com/spring/springboot/PathVariable_annotation/UserController.java
@@ -1,4 +1,17 @@
 package com.spring.springboot.PathVariable_annotation;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @GetMapping("/api/dummy/users/{userId}/post/{postId}")
+    public String searchUserPostWithMultiPathVariable(@PathVariable Long userId, @PathVariable   Long postId){
+        System.out.printf("Returned user and its post is %d and %d respectively",userId, postId, userId, postId );
+
+        return "Returned user and its post is at "+userId+" and "+postId+" respectively";
+    }
+
 }
