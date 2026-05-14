c08f9b09f554ee29f1e64e7e8635a3989a4c4efe
Sachin Sharma <codwork.tdotslash@gmail.com>
2026-05-15 00:00:26 +0530

    ref: slight ref in application.properties

---
+++ src/main/java/com/spring/springboot/p11_API_Versioning_latestSpring7Approach/ControllerforAll4Versioning/VersionController.java
@@ -0,0 +1,22 @@
+package com.spring.springboot.p11_API_Versioning_latestSpring7Approach.headerVersioning;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//Same as requestParamterVersioning, now we need not to change anything in this file but WebConfig is only the place to switch over the various versioning type
+@RestController
+@RequestMapping("/api/version")
+public class VersionController {
+
+    @GetMapping(version = "1")
+    ResponseEntity<String> defaultAPIversion(){
+     return ResponseEntity.ok("API Version 1.0.0");
+    }
+
+    @GetMapping(version = "2")
+    ResponseEntity<String> v2Version(){
+        return ResponseEntity.ok("API Version 2.0.0");
+    }
+
+}
---
+++ src/main/java/com/spring/springboot/p11_API_Versioning_latestSpring7Approach/WebConfig.java
@@ -0,0 +1,10 @@
+package com.spring.springboot.p11_API_Versioning_latestSpring7Approach;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.accept.ApiVersionResolver;
+import org.springframework.web.accept.PathApiVersionResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class Config implements WebMvcConfigurer {
+    
+}
