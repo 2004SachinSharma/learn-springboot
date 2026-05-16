1ca081e825b24a684949c531cf3059718112bf2f
Sachin Sharma <codwork.tdotslash@gmail.com>
2026-05-17 01:40:51 +0530

    add: learn H2 database in Spring, how it helps at the development stage and more...

--- src/main/java/com/spring/springboot/p12_H2Database/Main.java
+++ src/main/java/com/spring/springboot/p12_H2Database/Main.java
@@ -1,4 +1,12 @@
 package com.spring.springboot.p12_H2Database;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+
+    public static void main(String []args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
