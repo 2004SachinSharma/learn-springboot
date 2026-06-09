c0b7b79a080f106c5beb17ffc2bdb1409da547d3
Sachin Sharma <codwork.tdotslash@gmail.com>
2026-06-09 22:47:48 +0530

    feat: see a User friendly way to return the proper Error Response

--- src/main/java/com/spring/springboot/p14_BackendValidation/GlobalExceptionHandler.java
+++ src/main/java/com/spring/springboot/p14_BackendValidation/GlobalExceptionHandler.java
@@ -7,17 +7,32 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<ErrorResponseDto> handlerMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+//
+//
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDto(HttpStatus.BAD_REQUEST, exception.getMessage(), LocalDateTime.now()));
+//
+//    }
+
+    //    Use either of the Above one or the below one
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponseDto> handlerMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+    public ResponseEntity<Map<String, String>> handlerMethodArgumentNotValid(MethodArgumentNotValidException exception){
+        Map<String,String> errors = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors().forEach((fieldError) -> { errors.put(fieldError.getField(),fieldError.getDefaultMessage());});//The best way to return User friendly error messages
 
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDto(HttpStatus.BAD_REQUEST, exception.getMessage(), LocalDateTime.now()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 
     }
+
 
 }
