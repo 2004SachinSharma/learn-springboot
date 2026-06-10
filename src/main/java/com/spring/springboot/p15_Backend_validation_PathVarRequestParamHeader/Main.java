a1d70663d5318609ed52c55f026660f507766670
Sachin Sharma <codwork.tdotslash@gmail.com>
2026-06-10 23:17:03 +0530

    add: see full Dispatcher Servlet Lifecycle, how it goes and how exceptions are caught to GlobalExceptionHandling

---
+++ src/main/java/com/spring/springboot/p15_Backend_validation_PathVarRequestParamHeader/Main.java
@@ -0,0 +1,18 @@
+package com.spring.springboot.p15_Backend_validation_PathVarRequestParamHeader;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class Main {
+    static void main()
+    {
+
+      SpringApplication.run(Main.class);
+
+
+    }
+
+}
